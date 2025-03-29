f6f8e55b8270dce17efa1fbfa154978614c429d2
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-03-30 00:27:11 +0600

    text to image genarate api added and create a image

--- src/page/GenarateImage.jsx
+++ src/page/GenarateImage.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
 const GenarateImage = () => {
-    const handelClick = (e) => {
+
+
+    const handelSubmit = (e) => {
         e.preventDefault()
+        const prompt = e.target.prompt.value;
+        const form = new FormData()
+        form.append('prompt', prompt)
+        fetch('https://clipdrop-api.co/text-to-image/v1', {
+            method: 'POST',
+            headers: {
+                'x-api-key': import.meta.env.VITE_CD_KEY,
+            },
+            body: form,
+        }).then(response => response.arrayBuffer())
+            .then(buffer => {
+                const blob = new Blob([buffer], { type: "image/png" });
+                const url = URL.createObjectURL(blob);
+                console.log(url)
+            })
+
     }
     return (
         <div className='container mx-auto px-5 py-5'>
             <h3 className='text-3xl font-medium text-center'>Genarate an Image </h3>
             <form
-                onClick={handelClick}
+                onSubmit={handelSubmit}
                 className='place-items-center pt-20'>
 
                 <fieldset className="w-80">
@@ -16,6 +34,7 @@ const GenarateImage = () => {
                     <div className="join">
                         <input
                             type="text"
+                            name='prompt'
                             placeholder="type your message here"
                             className="input input-bordered join-item w-full" />
                         <button className="btn btn-primary join-item">Genarate</button>
